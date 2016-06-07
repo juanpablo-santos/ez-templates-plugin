f54a210fc2d14114a024702f4b936bbcace8843d
Marc Carter <drekbour@fastmail.fm>
2016-06-07 18:02:18 +0100

    Feature: Remove (project-level) dependency on matrix-project

--- src/main/java/com/joelj/jenkins/eztemplates/exclusion/MatrixAxisExclusion.java
+++ src/main/java/com/joelj/jenkins/eztemplates/exclusion/MatrixAxisExclusion.java
@@ -1,13 +1,16 @@
 package com.joelj.jenkins.eztemplates.exclusion;
 
+import com.google.common.base.Throwables;
 import com.joelj.jenkins.eztemplates.utils.ReflectionUtils;
-import hudson.matrix.AxisList;
-import hudson.matrix.MatrixBuild;
-import hudson.matrix.MatrixProject;
 import hudson.model.AbstractProject;
+import org.apache.commons.lang.ClassUtils;
+
+import java.lang.reflect.Method;
 
 public class MatrixAxisExclusion extends HardCodedExclusion {
-    private AxisList axes;
+
+    private static final String MATRIX_PROJECT = "hudson.matrix.MatrixProject";
+    private Object axes; // AxesList
 
     @Override
     public String getId() {
@@ -21,33 +24,46 @@ public class MatrixAxisExclusion extends HardCodedExclusion {
 
     @Override
     public void preClone(AbstractProject implementationProject) {
-        if (implementationProject instanceof MatrixProject) {
-            MatrixProject matrixProject = (MatrixProject) implementationProject;
-            axes = matrixProject.getAxes();
+        if (isMatrixProject(implementationProject)) {
+            axes = ReflectionUtils.getFieldValue(implementationProject.getClass(), implementationProject, "axes");
         }
     }
 
     @Override
     public void postClone(AbstractProject implementationProject) {
-        if (implementationProject instanceof MatrixProject) {
-            fixAxisList((MatrixProject) implementationProject, axes);
+        if (isMatrixProject(implementationProject)) {
+            fixAxisList(implementationProject, axes);
         }
     }
 
     /**
-     * Inlined from {@link MatrixProject#setAxes(hudson.matrix.AxisList)} except it doesn't call save.
+     * Inlined from MatrixProject#setAxes(hudson.matrix.AxisList) except it doesn't call save.
      *
      * @param matrixProject The project to set the Axis on.
      * @param axisList      The Axis list to set.
      */
-    private static void fixAxisList(MatrixProject matrixProject, AxisList axisList) {
+    private static void fixAxisList(AbstractProject matrixProject, Object axisList) {
         if (axisList == null) {
             return; //The "axes" field can never be null. So just to be extra careful.
         }
-        ReflectionUtils.setFieldValue(MatrixProject.class, matrixProject, "axes", axisList);
+        ReflectionUtils.setFieldValue(matrixProject.getClass(), matrixProject, "axes", axisList);
 
-        //noinspection unchecked
-        ReflectionUtils.invokeMethod(MatrixProject.class, matrixProject, "rebuildConfigurations", ReflectionUtils.MethodParameter.get(MatrixBuild.MatrixBuildExecution.class, null));
+        Class<?> clazz=null;
+        try {
+            clazz = Class.forName(MATRIX_PROJECT);
+            for (Method m : clazz.getDeclaredMethods()) {
+                if (m.getName().equals("rebuildConfigurations")) {
+                    hudson.util.ReflectionUtils.makeAccessible(m);
+                    hudson.util.ReflectionUtils.invokeMethod(m, matrixProject, new Object[] {null});
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            Throwables.propagate(e);
+        }
+    }
+
+    private static boolean isMatrixProject(AbstractProject project) {
+        return MATRIX_PROJECT.equals(project.getClass().getName());
     }
 
 }
